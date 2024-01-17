1538f876e310c59351188adb412c8e03d7ce11c2
sultan <salhmry339@gmail.com>
2024-01-17 23:21:45 +0300

    تم اضافة صفحة تعديل بيانات المستثمر

--- page/views.py
+++ page/views.py
@@ -27,5 +27,9 @@ def addpost(request):
     return render(request, 'pages/addpost.html')
 
 def promoreq(request):
-    return render(request, 'pages/promoreq.html')        
+    return render(request, 'pages/promoreq.html')
+
+def update(request):
+    return render(request, 'pages/update.html')
+
 
