6f33a085a12d7f22858dbd7da0982c6dbfde4cc4
bakr7777 <bkralmrysy12@gmail.com>
2024-02-03 01:35:03 +0300

    تم اضهار المشاريع من قاعدة البيانات وطلبات الترويج وتم عمل التطنيفات حسب  انواع تصنيفات المشروع

--- The_Owner/urls.py
+++ The_Owner/urls.py
@@ -1,8 +1,8 @@
-# from . import views
-# from django.urls import path
+from . import views
+from django.urls import path
 
 
-# urlpatterns = [
-#     path('', views.index , name='index'),
-#     path('about', views.about, name='about'),
-# ]
+urlpatterns = [
+    path('', views.index , name='index'),
+    path('about', views.about, name='about'),
+]
--- wasl/settings.py
+++ wasl/settings.py
@@ -220,10 +220,20 @@ STATICFILES_DIRS = [
 
 ]
 
+#MEDIA-----------------------------
+# # settings.py
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# في ملف إعدادات Django الخاص بك
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
