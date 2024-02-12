2c383608ed4595dca1fbc00f66f0db322b9a19ca
yousef2277 <youssif2277@gmail.com>
2024-02-12 12:58:43 -0900

    تم اضافة صفحة مفضلة والاحصائيات تم اضافة صفحة مفضلة والاحصائيات

--- page/views.py
+++ page/views.py
@@ -3,12 +3,21 @@ from The_Owner.models import *
 from .models import *
 from The_Investor.models import * 
 from FM.models import *
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+
 
 def index(request):
     projects = Project.objects.all()
     categories = ProjectCategory.objects.all()
     promo_requests = PromoRequest.objects.all()  # قم بتحميل طلبات الترويج
-    return render(request, 'pages/index.html', {'projects': projects, 'categories': categories, 'promo_requests': promo_requests})
+    total_projects = Project.objects.count()
+    total_investor = Investor.objects.count()
+    total_owners = Owner.objects.count()
+
+    return render(request, 'pages/index.html', {'projects': projects, 'categories': categories, 'promo_requests': promo_requests, 'total_projects': total_projects ,'total_investor': total_investor, 'total_owners': total_owners})
 
 
 # def index(request):
@@ -56,9 +65,23 @@ def project(request):
     return render(request, 'pages/project.html')
 
 def prodesc(request):
+    if request.method == 'POST':
+        projectid = request.POST.get('project')
+        investorid = request.POST.get('investor')
+
+        project = Project.objects.get(id = projectid)
+        investor =  Investor.objects.get(id = investorid)
+
+
+        Favorite.objects.get_or_create(investor=investor , project=project)
+        return redirect('favorite')
+       
     project = Project.objects.all()
     investment_request = InvestmentRequest.objects.all()
     return render(request, 'pages/prodesc.html', {'project': project,'investment_request': investment_request})
 
 
-
+def favorite(request):
+     project = Project.objects.all()
+     favorite = Favorite.objects.all()
+     return render(request, 'pages/favorite.html', {'project': project,'favorite': favorite}) 
