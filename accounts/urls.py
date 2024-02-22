8b3dbaeb7557590ede9e85acd4729fa687c0d6a4
youns <ywnklan@gmail.com>
2024-02-22 03:20:03 +0300

    تم اكمال عملية انشاء الحسابات وتسجيل الدخول وتعديل الحسابات

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('signup', views.signup, name='signup'),
+    path('profile', views.profile, name='profile'),
+]
